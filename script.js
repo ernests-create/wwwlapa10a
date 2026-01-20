705b24bebc8df2cdf76a1f14f61119d142ee3aca
ernests-create <ernests.korde@gmail.com>
2026-01-20 10:30:36 +0200

    ievads
    ievads

---
+++ script.js
@@ -0,0 +1,11 @@
+function poga1()
+{ window.location.href="Parise.html" } 
+
+function poga2()
+{ window.location.href="roma.html" } 
+
+function poga3()
+{ window.location.href="griekija.html" } 
+
+function poga4()
+{ window.location.href="" }
